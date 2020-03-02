45cef9342fa2a3d95258478643f1b77b6b171da7
nurjaini <aininj847@gmail.com>
2020-03-02 17:48:27 +0700

    ui fragment event dan ui fragment profile

---
+++ app/src/main/java/app/codelabs/forum/activities/event/EventActivity.java
@@ -0,0 +1,14 @@
+package app.codelabs.forum.activities.event;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+    }
+}
