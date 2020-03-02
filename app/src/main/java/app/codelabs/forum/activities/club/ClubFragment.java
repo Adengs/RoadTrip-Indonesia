a4dcc196621cf9679c669278b59f321d22ce706f
nelli <nellis270996@gmail.com>
2020-03-02 17:50:30 +0700

    Merge branch 'master' of http://139.59.232.60/edinofri/forum-app
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/app/codelabs/forum/activities/club/ClubFragment.java
+++ app/src/main/java/app/codelabs/forum/activities/club/ClubFragment.java
@@ -1,14 +1,17 @@
 package app.codelabs.forum.activities.club;
 
 
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import app.codelabs.forum.R;
 
 /**
@@ -16,7 +19,9 @@ import app.codelabs.forum.R;
  */
 public class ClubFragment extends Fragment {
 
-
+    RecyclerView recyclerView;
+    private RvAdapter adapter;
+    Context context;
     public ClubFragment() {
         // Required empty public constructor
     }
@@ -26,7 +31,16 @@ public class ClubFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_club2, container, false);
+        return inflater.inflate(R.layout.fragment_club, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        adapter = new RvAdapter();
+        recyclerView = view.findViewById(R.id.rv_Club);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.setAdapter(adapter);
+    }
 }
