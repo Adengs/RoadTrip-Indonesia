ceb1e8c470bfcc37555d816cc0bf66169442f8c2
adella <adellazhang18@gmail.com>
2020-03-04 17:53:16 +0700

    HomeActivity tampilan setelah login dan register

---
+++ app/src/main/java/app/codelabs/forum/activities/home/HomeActivityBig.java
@@ -0,0 +1,14 @@
+package app.codelabs.forum.activities.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivityBig extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_big);
+    }
+}
---
+++ app/src/main/java/app/codelabs/forum/activities/home/notivication/NotivicationHome.java
@@ -0,0 +1,14 @@
+package app.codelabs.forum.activities.home.notivication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotivicationHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notivication_home);
+    }
+}
