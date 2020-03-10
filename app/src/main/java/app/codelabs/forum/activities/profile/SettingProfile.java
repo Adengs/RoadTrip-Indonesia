51c868f75e820ddba39c81fed8662453cd5f94c0
nurjaini <aininj847@gmail.com>
2020-03-10 17:08:06 +0700

    membuat tombol back pada setting profile dan juga event activity

--- app/src/main/java/app/codelabs/forum/activities/profile/SettingProfile.java
+++ app/src/main/java/app/codelabs/forum/activities/profile/SettingProfile.java
@@ -1,17 +1,22 @@
 package app.codelabs.forum.activities.profile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import app.codelabs.forum.R;
+import app.codelabs.forum.activities.home.HomeActivity;
 import app.codelabs.forum.activities.login.LoginActivity;
+import app.codelabs.forum.activities.profile.fragment.ProfileFragment;
 
 public class SettingProfile extends AppCompatActivity {
     Button btnLogoutSetPro;
+    TextView backsettingPro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,12 @@ public class SettingProfile extends AppCompatActivity {
     }
 
     private void setEvent() {
+        backsettingPro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingProfile.this , HomeActivity.class));
+            }
+        });
         btnLogoutSetPro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,6 +45,6 @@ public class SettingProfile extends AppCompatActivity {
     }
 
     private void setView() {
-
+        backsettingPro=findViewById(R.id.back_settingProfile);
         btnLogoutSetPro=findViewById(R.id.btnlogoutSettingP); }
 }
