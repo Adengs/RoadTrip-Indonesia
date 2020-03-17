ed5ff8492a72fa0d6330fb20d0624f106283407e
adella <adellazhang18@gmail.com>
2020-03-17 11:32:28 +0700

    done UI by Adella

---
+++ app/src/main/java/app/codelabs/forum/helpers/Session.java
@@ -0,0 +1,22 @@
+package app.codelabs.forum.helpers;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Session {
+    private SharedPreferences sharedPreferences;
+    private Context context;
+
+    public Session(Context context){
+        sharedPreferences = context.getSharedPreferences("Settings",Context.MODE_PRIVATE);
+    }
+    public static Session init(Context context){
+        return new Session(context);
+    }
+
+    public String getAppToken(){
+        String apptoken = sharedPreferences.getString("apptoken","");
+        return  apptoken;
+    }
+
+}
