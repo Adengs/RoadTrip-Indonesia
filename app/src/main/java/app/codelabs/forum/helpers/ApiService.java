a5d7f088508696133713f5ce00197377343b81b8
adella <adellazhang18@gmail.com>
2020-03-17 12:16:52 +0700

    API Adel

--- app/src/main/java/app/codelabs/forum/helpers/ApiService.java
+++ app/src/main/java/app/codelabs/forum/helpers/ApiService.java
@@ -1,5 +1,15 @@
 package app.codelabs.forum.helpers;
 
+import java.util.Map;
+
+import app.codelabs.forum.models.ResponseApi;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
 public interface ApiService {
+
+    @POST("api/app_token")
+    Call<ResponseApi>apptoken(@Body Map<String,String> body);
 
 }
