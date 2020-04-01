ad234c370c5e930739ab13d748960dcdbe13059a
Edinofri <tukangbasic@gmail.com>
2020-04-01 13:06:37 +0700

    add interceptor log

--- app/src/main/java/app/codelabs/forum/helpers/ConnectionApi.java
+++ app/src/main/java/app/codelabs/forum/helpers/ConnectionApi.java
@@ -1,13 +1,21 @@
 package app.codelabs.forum.helpers;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ConnectionApi {
     public static ApiService apiService(){
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+        httpClient.addInterceptor(logging);
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://104.248.149.236/qwerty/forum/service/gateway/public/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
                 .build();
         return retrofit.create(ApiService.class);
     }
